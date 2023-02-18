ef4b30ad27529a99fc9840dad1b893aea2928875
Ilya Gomanyuk <iliagmn@gmail.com>
2023-02-18 17:16:39 +0500

    Create 3535.py

---
+++ 3535.py
@@ -0,0 +1,29 @@
+n=int(input("Введите число n: "))
+
+a=[]
+b=[]
+s=1
+for i in range (1, n+1):
+    s = i*(i+1)
+    b.append(s)
+    y1 = str(i)
+    a.append(y1)
+    a.append('*')
+    y2 = str(i+1)
+    a.append(y2)
+    a.append('+')
+    
+summ = sum(b)
+summ_str = str(summ) 
+   
+st = ""
+for word in a:
+    st += str(word)
+    
+st=st[:-1]
+
+summ2 = st+'='+summ_str
+print(summ2)
+    
+    
+
